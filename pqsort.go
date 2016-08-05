d9b8820239c04f07946c40aed8464c10a70efc98
K <x00.x7f@gmail.com>
2016-08-05 18:58:38 +0900

    Modified

--- pqsort.go
+++ pqsort.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	_ "net/http/pprof"
+	"runtime"
 	"sync"
 )
 
@@ -11,15 +11,28 @@ func main() {
 	max := 10000000
 	a := createAry(max)
 	fmt.Println(a)
+	Dispersion(a)
+}
+
+func Dispersion(a []int) {
+	cpu := runtime.NumCPU()
+	runtime.GOMAXPROCS(cpu)
+	length := len(a) / cpu
 
 	var wg sync.WaitGroup
-	pqsort(a, &wg)
-	wg.Wait()
+
+	wg.Add(cpu)
+	for i := 0; i < cpu; i++ {
+		go func(i int) {
+			qsort(a[i*length : (i+1)*length])
+			wg.Done()
+		}(i)
+	}
 
 	fmt.Println(a)
 }
 
-func pqsort(ary []int, wg *sync.WaitGroup) {
+func qsort(ary []int) {
 	if len(ary) < 2 {
 		return
 	}
@@ -44,20 +57,58 @@ func pqsort(ary []int, wg *sync.WaitGroup) {
 	ary[left], ary[right] = ary[right], ary[left]
 
 	// Go down the rabbit hole
-	wg.Add(2)
-	go func() {
-		pqsort(ary[:left], wg)
-		wg.Done()
-	}()
-	go func() {
-		pqsort(ary[left+1:], wg)
-		wg.Done()
-	}()
+	qsort(ary[:left])
+	qsort(ary[left+1:])
 }
 
-func createAry(max int) (ary []int) {
-	for i := 0; i < max; i++ {
-		ary = append(ary, rand.Int()%max)
+func Parallel(a []int) {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	ch := make(chan []int)
+	go pqsort(a, ch)
+	fmt.Println(<-ch)
+}
+
+func pqsort(ary []int, ch chan []int) {
+	if len(ary) < 2 {
+		ch <- []int{}
+		return
 	}
-	return
+
+	left, right := 0, len(ary)-1
+
+	// Pick a pivot
+	pivotIndex := rand.Int() % len(ary)
+
+	// Move the pivot to the right
+	ary[pivotIndex], ary[right] = ary[right], ary[pivotIndex]
+
+	// Pile elements smaller than the pivot on the left
+	for i := range ary {
+		if ary[i] < ary[right] {
+			ary[i], ary[left] = ary[left], ary[i]
+			left++
+		}
+	}
+
+	// Place the pivot after the last smaller element
+	ary[left], ary[right] = ary[right], ary[left]
+
+	// Go down the rabbit hole
+	ch1 := make(chan []int)
+	ch2 := make(chan []int)
+	go pqsort(ary[:left], ch1)
+	go pqsort(ary[left+1:], ch2)
+	<-ch1
+	<-ch2
+	close(ch1)
+	close(ch2)
+	ch <- ary
+}
+
+func createAry(max int) []int {
+	ary := make([]int, max)
+	for i := 0; i < max; i++ {
+		ary[i] = rand.Int() % max
+	}
+	return ary
 }
--- qsort.go
+++ qsort.go
@@ -1,52 +1,53 @@
 package main
 
 import (
-  "fmt"
-  "math/rand"
+    "fmt"
+    "math/rand"
 )
 
 func main() {
-  max := 10000000
-  a := createAry(max)
-  fmt.Println(a)
+    max := 10000000
+    a := createAry(max)
+    fmt.Println(a)
 
-  fmt.Println(qsort(a))
+    qsort(a)
+
+    fmt.Println(a)
 }
 
-func qsort(a []int) []int {
-  if len(a) < 2 {
-    return a
-  }
-
-  left, right := 0, len(a)-1
-
-  // Pick a pivot
-  pivotIndex := rand.Int() % len(a)
-
-  // Move the pivot to the right
-  a[pivotIndex], a[right] = a[right], a[pivotIndex]
-
-  // Pile elements smaller than the pivot on the left
-  for i := range a {
-    if a[i] < a[right] {
-      a[i], a[left] = a[left], a[i]
-      left++
+func qsort(a []int) {
+    if len(a) < 2 {
+        return
     }
-  }
 
-  // Place the pivot after the last smaller element
-  a[left], a[right] = a[right], a[left]
+    left, right := 0, len(a)-1
 
-  // Go down the rabbit hole
-  qsort(a[:left])
-  qsort(a[left+1:])
+    // Pick a pivot
+    pivotIndex := rand.Int() % len(a)
 
-  return a
+    // Move the pivot to the right
+    a[pivotIndex], a[right] = a[right], a[pivotIndex]
+
+    // Pile elements smaller than the pivot on the left
+    for i := range a {
+        if a[i] < a[right] {
+            a[i], a[left] = a[left], a[i]
+            left++
+        }
+    }
+
+    // Place the pivot after the last smaller element
+    a[left], a[right] = a[right], a[left]
+
+    // Go down the rabbit hole
+    qsort(a[:left])
+    qsort(a[left+1:])
 }
 
-func createAry(max int) (ary []int) {
-  for i := 0; i < max; i++ {
-    ary = append(ary, rand.Int()%max)
-  }
-  return
+func createAry(max int) []int {
+    ary := make([]int, max)
+    for i := 0; i < max; i++ {
+        ary[i] = rand.Int() % max
+    }
+    return ary
 }
